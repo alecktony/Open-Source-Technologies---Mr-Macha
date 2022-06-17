29a5f27fe3250a2383af3c6b860be795a6925ebf
steven5036 <stefangabriel120@gmail.com>
2022-06-17 11:13:41 +0300

    just made changes to the ui

--- login-utils/login.c
+++ login-utils/login.c
@@ -25,7 +25,9 @@
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * steven made changes to this folder
  */
+
 #include <sys/param.h>
 #include <stdio.h>
 #include <ctype.h>
